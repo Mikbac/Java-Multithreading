e90531a9902d8df388d1c5e11b61e6fa97aa3bb3
MikBac <mbachorz1997@gmail.com>
2024-10-20 18:58:03 +0200

    update 81 example

---
+++ examples/src/main/java/pl/mikbac/threads/Example81/Controller/TestController.java
@@ -0,0 +1,39 @@
+package pl.mikbac.threads.Example81.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by MikBac on 19.10.2024
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final TestService testService;
+
+    @PostMapping("/counter/atomic")
+    public ResponseEntity<Long> atomicIncrementAndGet() {
+        return ResponseEntity.ok(testService.atomicCounter());
+    }
+
+    @GetMapping("/counter/atomic")
+    public ResponseEntity<Long> atomicGet() {
+        return ResponseEntity.ok(testService.getAtomicCounter());
+    }
+
+    @PostMapping("/counter/standard")
+    public ResponseEntity<Long> standardIncrementAndGet() {
+        return ResponseEntity.ok(testService.standardCounter());
+    }
+
+    @GetMapping("/counter/standard")
+    public ResponseEntity<Long> standarGet() {
+        return ResponseEntity.ok(testService.getStandardCounter());
+    }
+
+}
--- examples/src/main/java/pl/mikbac/threads/Example81/README.md
+++ examples/src/main/java/pl/mikbac/threads/Example81/README.md
@@ -1,7 +1,17 @@
 # Atomic variables
 
 ```shell
-curl --request POST \
-  --url http://localhost:8080/counter
+curl http://localhost:8080/counter/standard
+curl http://localhost:8080/counter/atomic
+# 0
+# 0
+
+docker run --rm williamyeh/hey -n 2000 -c 10 -m POST http://host.docker.internal:8080/counter/standard
+docker run --rm williamyeh/hey -n 2000 -c 10 -m POST http://host.docker.internal:8080/counter/atomic
+
+curl http://localhost:8080/counter/standard
+curl http://localhost:8080/counter/atomic
+# 1949
+# 2000
 ```
 
